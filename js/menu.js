5ee31e735aa4fd42fa018ebccd6893f40e1f1bc3
fcojr <fco-jr@live.com>
2017-11-17 19:49:48 -0200

    menu mobile

--- js/menu.js
+++ js/menu.js
@@ -1,5 +1,13 @@
 $(document).ready(function(){
+	$(".nav-back").hide();
 	$('.fa-bars').on("click", function(){
 		$('#nav').toggleClass("menu-ativo");
+		if($('#nav').hasClass("menu-ativo")){
+			$(".nav-back").show();
+		}
+		$(".nav-back").on("click", function(){
+			$('#nav').removeClass("menu-ativo");
+			$(".nav-back").hide();
+		})
 	})
 })
